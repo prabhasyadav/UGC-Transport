5703eaccf39f886e9e50e940f6fc5329f2c7ad75
prabhasyadav <prabhasyadav@users.noreply.github.com>
2016-12-11 00:48:30 +0530

    Add files via upload

--- transport2D.m
+++ transport2D.m
@@ -10,17 +10,19 @@ lam =0 %lamda
 xx = x - v*t;
 c = (M/4/pi/t/sqrt(Dx*Dy))*ones(100).*exp((-0.25*(xx.*xx/Dx + y.*y/Dy)/t)-lam*t);
 
-figure; % unfilled contour
+%figure; % unfilled contour
+subplot(2,1,1)
 contour(x,y,c,5, 'ShowText','on');  
 xlabel ('x'); ylabel ('y'); 
-%
-%
-figure; % filled contour
-contourf(x,y,c,5,'ShowText','on');  
-xlabel ('x'); ylabel ('y'); 
+title('Concentration Contour at t = 1 d');
+%figure; % filled contour
+%contourf(x,y,c,5,'ShowText','on');  
+%xlabel ('x'); ylabel ('y'); 
 
-figure;
+%figure;
+subplot(2,1,2)
 surfc(x,y,c); 
-colorbar; xlabel ('x'); ylabel ('y'); 
+colorbar; xlabel ('x'); ylabel ('y');
+zlabel("C");
 
  
